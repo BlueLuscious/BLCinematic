fd23c791dddb512a6796ce3e360b597fb656f3b3
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-11-09 16:23:14 -0300

    update
    index.html:
    - update trending_section and create movies_section.
    
    index.css:
    - added display to movies_section
    
    index.js:
    - get api data from movies/now_playing

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ window.addEventListener('DOMContentLoaded', function () {
     // const apiKey = 'cfac909fcba4ab5a5293e810bfda6d52'
     const IMAGE_URL = 'https://www.themoviedb.org/t/p/w220_and_h330_face'
     const trendingParam = 'https://api.themoviedb.org/3/trending/all/day?language=en-US'
+    const moviesNowPlayingParam = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1'
 
     const trendingCard = document.getElementById('trendingTable')
+    const nowPlayingCard = document.getElementById('moviesTable')
 
     getApiData(trendingParam)
         .then(data => {
-
             const trendingList = data.results
 
             trendingList.forEach(trendingItem => {
@@ -20,11 +21,11 @@ window.addEventListener('DOMContentLoaded', function () {
                 trendingImage.src = `${IMAGE_URL + trendingItem.poster_path}`
                 trendingImageData.appendChild(trendingImage)
 
-                /* movies title */
+                // movies title
                 const trendingTitle = document.createElement('td')
                 trendingTitle.textContent = trendingItem.title
 
-                /* tv shows and series name */
+                // tv shows and series name
                 const trendingName = document.createElement('td')
                 trendingName.textContent = trendingItem.name
 
@@ -32,15 +33,40 @@ window.addEventListener('DOMContentLoaded', function () {
                 trendingRow.appendChild(trendingTitle)
                 trendingRow.appendChild(trendingName)
                 trendingCard.appendChild(trendingRow)
-
             })
+        })
 
+    getApiData(moviesNowPlayingParam)
+        .then(data => {
+            const moviesNowPlayingList = data.results
+
+            moviesNowPlayingList.forEach(nowPlayingItem => {
+                const nowPlayingRow = document.createElement('tr')
+                nowPlayingRow.classList.add('main_now_playing_tr')
+
+                const nowPlayingImageData = document.createElement('td')
+                const nowPlayingImage = document.createElement('img')
+                nowPlayingImage.src = `${IMAGE_URL + nowPlayingItem.poster_path}`
+                nowPlayingImageData.appendChild(nowPlayingImage)
+
+                // movies title
+                const nowPlayingTitle = document.createElement('td')
+                nowPlayingTitle.textContent = nowPlayingItem.title
+
+                // tv shows and series name
+                const nowPlayingName = document.createElement('td')
+                nowPlayingName.textContent = nowPlayingItem.name
+
+                nowPlayingRow.appendChild(nowPlayingImage)
+                nowPlayingRow.appendChild(nowPlayingTitle)
+                nowPlayingRow.appendChild(nowPlayingName)
+                nowPlayingCard.appendChild(nowPlayingRow)
+            })
         })
 
 
 
-
-    /* GET APIDATA FUNCTION  */
+    // GET APIDATA FUNCTION
     async function getApiData(params) {
         const options = {
             method: 'GET',
@@ -58,6 +84,6 @@ window.addEventListener('DOMContentLoaded', function () {
             })
             .catch(err => console.error(err))
     }
-    /* GET APIDATA FUNCTION  */
+    // GET APIDATA FUNCTION
 
 })
