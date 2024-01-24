e61d0c93ac476bd8d615c5a71a4404e3f5114a5a
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-01-24 01:44:22 -0300

    update JS folder
    index.js:
    - created an event listener to refresh the page when it's above and below 1024px.
    
    apiData.js:
    - created handleApiData method in ApiHandlers class.
    
    getTrending.js, getMovies.js & getSeries.js:
    - the functions of these files are now all in one.
    (handleApiData method in ApiHandlers class).
    - optimized codes.

--- JS/getTrending.js
+++ JS/getTrending.js
@@ -1,4 +1,4 @@
-import { ApiUrls, CardClassName, Pathname, Template } from "./constants.js"
+import { ApiUrls, CardClassName } from "./constants.js"
 import { ApiHandlers } from "./apiData.js"
 
 const api = new ApiHandlers
@@ -16,30 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const seriesByIndexCard = document.getElementById('seriesByIndex')
     const cardsByIndex = [moviesByIndexCard, seriesByIndexCard]
 
-    const urlParams = new URLSearchParams(window.location.search)
-    const indexCard = urlParams.get('indexCard')
-
-    handleTrendingData(ApiUrls.trendingAllDay, trendingAllDayCard, cardsByIndex, 'trending_all_day', indexCard)
-    handleTrendingData(ApiUrls.trendingMoviesDay, trendingMoviesDayCard, cardsByIndex, 'trending_movie_day', indexCard)
-    handleTrendingData(ApiUrls.trendingSeriesDay, trendingSeriesDayCard, cardsByIndex, 'trending_serie_day', indexCard)
-    handleTrendingData(ApiUrls.trendingAllWeek, trendingAllWeekCard, cardsByIndex, 'trending_all_week', indexCard)
-    handleTrendingData(ApiUrls.trendingMoviesWeek, trendingMoviesWeekCard, cardsByIndex, 'trending_movie_week', indexCard)
-    handleTrendingData(ApiUrls.trendingSeriesWeek, trendingSeriesWeekCard, cardsByIndex, 'trending_serie_week', indexCard)
-
-    function handleTrendingData(apiUrl, cardAll, cardsByIndex, category, indexCard) {
-        if (window.location.pathname === Pathname.HTML_FOLDER + Template.INDEX_TEMPLATE ||
-            window.location.search === `?${category}&indexCard=${indexCard}`
-        ) {
-            api.getApiDataByURL(apiUrl).then(data => {
-                if (window.location.pathname === Pathname.HTML_FOLDER + Template.INDEX_TEMPLATE) {
-                    api.getResults(data.results, cardAll)
-                    api.getClickedCard(data.results, cardAll, CardClassName.TRENDING_CLASS, category)
-                }
-                if (indexCard) {
-                    api.getResultsByIndexcard(data.results[indexCard], cardsByIndex)
-                }
-            })
-        }
-    }
+    api.handleApiData(
+        ApiUrls.trendingAllDay, trendingAllDayCard, cardsByIndex, 'trending_all_day', null, CardClassName.TRENDING_CLASS
+    )
+    api.handleApiData(
+        ApiUrls.trendingMoviesDay, trendingMoviesDayCard, cardsByIndex, 'trending_movie_day', null, CardClassName.TRENDING_CLASS
+    )
+    api.handleApiData(
+        ApiUrls.trendingSeriesDay, trendingSeriesDayCard, cardsByIndex, 'trending_serie_day', null, CardClassName.TRENDING_CLASS
+    )
+    api.handleApiData(
+        ApiUrls.trendingAllWeek, trendingAllWeekCard, cardsByIndex, 'trending_all_week', null, CardClassName.TRENDING_CLASS
+    )
+    api.handleApiData(
+        ApiUrls.trendingMoviesWeek, trendingMoviesWeekCard, cardsByIndex, 'trending_movie_week', null, CardClassName.TRENDING_CLASS
+    )
+    api.handleApiData(
+        ApiUrls.trendingSeriesWeek, trendingSeriesWeekCard, cardsByIndex, 'trending_serie_week', null, CardClassName.TRENDING_CLASS
+    )
 
 })
