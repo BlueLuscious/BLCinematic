3e121f6de13687d1bd0ed62569bc778baf252bf0
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-01-30 03:55:14 -0300

    update JS folder
    index.js:
    - added removeAfterFromTables method from Toolbox class to remove the ::after from content tables.
    
    constants.js:
    - added some image-urls to the ApiUrls class.
    - added two classnames to the CardClassName class. (cardByIndex classnames).
    
    handlers.js:
    - added removeAfterFromTables method to Toolbox class.
    
    apiData.js:
    - ApiHandlers class:
       --> getResultsByIndexcard method was reformed.
    - ApiToolbox class:
       --> setCardImage method was reformed.
       --> setCardTitleAndDate method was divided into two methods, setCardDate and setCardTitle.
    
    getTrending.js, getMovies.js & getSeries.js:
    - maximum code optimization.

--- JS/getMovies.js
+++ JS/getMovies.js
@@ -5,13 +5,9 @@ const api = new ApiHandlers
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const moviesByIndexCard = document.getElementById('moviesByIndex')
-    const seriesByIndexCard = document.getElementById('seriesByIndex')
-    const cardsByIndex = [moviesByIndexCard, seriesByIndexCard]
-
-    api.handleApiData(ApiUrls.moviesNowPlaying, cardsByIndex, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
-    api.handleApiData(ApiUrls.moviesPopular, cardsByIndex, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
-    api.handleApiData(ApiUrls.moviesTopRated, cardsByIndex, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
-    api.handleApiData(ApiUrls.moviesUpcoming, cardsByIndex, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
+    api.handleApiData(ApiUrls.moviesNowPlaying, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
+    api.handleApiData(ApiUrls.moviesPopular, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
+    api.handleApiData(ApiUrls.moviesTopRated, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
+    api.handleApiData(ApiUrls.moviesUpcoming, Template.MOVIES_TEMPLATE, CardClassName.MOVIES_CLASS)
 
 })
--- JS/getTrending.js
+++ JS/getTrending.js
@@ -5,15 +5,11 @@ const api = new ApiHandlers
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const moviesByIndexCard = document.getElementById('moviesByIndex')
-    const seriesByIndexCard = document.getElementById('seriesByIndex')
-    const cardsByIndex = [moviesByIndexCard, seriesByIndexCard]
-
-    api.handleApiData(ApiUrls.trendingAllDay, cardsByIndex, null, CardClassName.TRENDING_CLASS)
-    api.handleApiData(ApiUrls.trendingMoviesDay, cardsByIndex, null, CardClassName.TRENDING_CLASS)
-    api.handleApiData(ApiUrls.trendingSeriesDay, cardsByIndex, null, CardClassName.TRENDING_CLASS)
-    api.handleApiData(ApiUrls.trendingAllWeek, cardsByIndex, null, CardClassName.TRENDING_CLASS)
-    api.handleApiData(ApiUrls.trendingMoviesWeek, cardsByIndex, null, CardClassName.TRENDING_CLASS)
-    api.handleApiData(ApiUrls.trendingSeriesWeek, cardsByIndex, null, CardClassName.TRENDING_CLASS)
+    api.handleApiData(ApiUrls.trendingAllDay, null, CardClassName.TRENDING_CLASS)
+    api.handleApiData(ApiUrls.trendingMoviesDay, null, CardClassName.TRENDING_CLASS)
+    api.handleApiData(ApiUrls.trendingSeriesDay, null, CardClassName.TRENDING_CLASS)
+    api.handleApiData(ApiUrls.trendingAllWeek, null, CardClassName.TRENDING_CLASS)
+    api.handleApiData(ApiUrls.trendingMoviesWeek, null, CardClassName.TRENDING_CLASS)
+    api.handleApiData(ApiUrls.trendingSeriesWeek, null, CardClassName.TRENDING_CLASS)
 
 })
